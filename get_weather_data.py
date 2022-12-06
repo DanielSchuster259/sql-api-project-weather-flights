f78dee6aa468ac0d72f6f0fa49c2c059d3f2b8eb
DanielSchuster259 <d.schuster259@gmail.com>
2022-12-06 09:49:21 +0100

    get_weather_data hourly

--- get_weather_data.py
+++ get_weather_data.py
@@ -1,7 +1,7 @@
 # Get weather data from August 2005 (during Hurricane Katrina), from weather stations nearest to 
 # lat and lon given as input variables
 
-def get_katrina_weather_data(lat, lon):
+def get_katrina_weather_data(lat, lon, granularity='daily'):
     
     from dotenv import load_dotenv
     load_dotenv()
@@ -9,7 +9,7 @@ def get_katrina_weather_data(lat, lon):
     import requests
     import os
 
-    url = "https://meteostat.p.rapidapi.com/point/daily"
+    url = (f"https://meteostat.p.rapidapi.com/point/{granularity}")
 
     querystring = {"lat":lat,"lon":lon,"start":"2005-08-01","end":"2005-08-31","alt":"2"}
 
@@ -20,17 +20,11 @@ def get_katrina_weather_data(lat, lon):
 
     response = requests.request("GET", url, headers=headers, params=querystring)
 
-    # Import json package
-    import json
     import pandas as pd
 
     weather_katrina = response.json()
-
-    #display(weather_katrina)
-
-    weather_katrina_data = weather_katrina['data']
-
-    weather_katrina_data_norm = pd.json_normalize(weather_katrina_data, sep="_")
+    weather_katrina_data = weather_katrina['data']                               # Retrieving only the nested data dict from .json file
+    weather_katrina_data_norm = pd.json_normalize(weather_katrina_data, sep="_") # Flattening data and convert to dataframe
 
     return weather_katrina_data_norm
 
@@ -47,10 +41,18 @@ def clean_hourly_data(df, airport_code):
 
 def clean_daily_data(df, airport_code):
 
-    df = df.drop(columns=['wdir', 'snow', 'tsun', 'wpgt', 'prcp', 'tmin', 'tmax'], axis=1)
-    df.rename(columns={'time' : 'date', 'temp' : 'temp_celsius', 'wspd' : 'wind_speed_kph', 'pres' : 'air_pressure_hPa', 'tavg' : 'avg_temp_celsius'}, inplace=True, errors='ignore')
+    df = df.drop(columns=['wdir', 'snow', 'tsun',       # Dropping columns that are unnecessary 
+                          'wpgt', 'prcp', 'tmin',       # or have 0 Non-Null values
+                          'tmax'], axis=1)
+    df.rename(columns={'time' : 'date', 
+                       'temp' : 'temp_celsius',         # Renaming columns
+                       'wspd' : 'wind_speed_kph', 
+                       'pres' : 'air_pressure_hPa', 
+                       'tavg' : 'avg_temp_celsius'}, 
+                       inplace=True, 
+                       errors='ignore')
 
-    airport_codes = [airport_code] * len(df)
+    airport_codes = [airport_code] * len(df)            # Creating a new column with airport code
     df['airport_code'] = airport_codes
 
     return df
